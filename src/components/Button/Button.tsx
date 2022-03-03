db2db2621ece27323c87b19e53f43a4a67285276
MurilloTSouza <murillotsouza@gmail.com>
2022-03-03 05:46:37 -0300

    Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,19 @@
+import React, { MouseEventHandler } from 'react';
+
+import './Button.css';
+
+interface ButtonProps {
+    onClick?: MouseEventHandler<HTMLButtonElement>
+}
+
+export const Button: React.FC<ButtonProps> = (props) => {
+    
+    const {onClick, children} = props;
+
+    return (
+        <button onClick={onClick} className='Button'>
+            {children}
+        </button>
+    )
+}
+
