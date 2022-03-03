cb64e17e516471a11ede114cb972b6c6ee001f4d
MurilloTSouza <murillotsouza@gmail.com>
2022-03-03 16:57:38 -0300

    passing controlled value to parent

--- src/components/NumberSpinner/NumberSpinner.tsx
+++ src/components/NumberSpinner/NumberSpinner.tsx
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import './NumberSpinner.css';
 import { SpinnerButton } from './SpinnerButton/SpinnerButton';
 
+import './NumberSpinner.css';
+
 interface NumberSpinnerProps {
-    defaultValue?: number
+    value: number,
+
+    onChange?: (value:number) => void,
 }
 
 export const NumberSpinner :React.FC<NumberSpinnerProps> = (props) => {
+
+    const {value} = props;
+    const onChange = props.onChange 
+        ? props.onChange         // if a function is defined set it to onChange
+        : (value :number) => {}; // if not set a empty function to avoid checking
 
     const MIN = 0; const MAX = 999;
     const clampValue = (value: number) => {
@@ -15,25 +23,19 @@ export const NumberSpinner :React.FC<NumberSpinnerProps> = (props) => {
         if (value > MAX) { value = MAX; }
         return value;
     }
-
-    // initialize value
-    var defaultValue = props.defaultValue 
-        ? clampValue(props.defaultValue) // clamp value before initialize
-        : 0; // 0 if not specified
     
-    var [value, setValue] = useState(defaultValue);
-
-    // common value seters
-    const setClampValue = (value: number) => {
-        setValue( clampValue(value) );
+    // use this function when passing unkown numbers
+    // instead of onChange, to clamp all values
+    const handleOnChange = (value: number) => {
+        onChange( clampValue(value) );
     }
-    const increment = () => { setClampValue(value+1) }
-    const decrement = () => { setClampValue(value-1) }
+    const increment = () => { handleOnChange(value+1) }
+    const decrement = () => { handleOnChange(value-1) }
 
     // input type is number, but besides numbers, 
     // '+' and '-'are still allowed to be typed,
     // so it will be prevented by the key handler function
-    const handleKeyPress =
+    const handleInputKeyPress =
         (event: React.KeyboardEvent<HTMLInputElement>) => {
 
         const reg = new RegExp("/+|-/");
@@ -43,21 +45,20 @@ export const NumberSpinner :React.FC<NumberSpinnerProps> = (props) => {
     // using handleChange to allow user to type values directly,
     // but when keyPress is prevented a empty value is returned
     // so it need to be filtered
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const inputValue = event.target.value;
 
         // if empty string because of backspace
-        if(inputValue==='') { setValue(0); }
+        if(inputValue==='') { onChange(0); }
 
-        // if not set clamped value(need to be parsed first)
+        // if not, set clamped value(need to be parsed first)
         else {
             var parsed = parseInt(inputValue);
-            setClampValue(parsed);    
+            handleOnChange(parsed);    
         }
 
         // update input value based on controlled value
         event.target.value = value.toString();
-        
     }
 
     return (
@@ -67,8 +68,8 @@ export const NumberSpinner :React.FC<NumberSpinnerProps> = (props) => {
             </SpinnerButton>
 
             <input type='number' value={value} 
-                onChange={handleChange} 
-                onKeyPress={handleKeyPress}/>
+                onChange={handleInputChange} 
+                onKeyPress={handleInputKeyPress}/>
 
             <SpinnerButton onClick={increment} disabled={value >= MAX}>
                 +
