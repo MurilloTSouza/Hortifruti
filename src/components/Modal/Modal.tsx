2e778a1b26b8505b83966a3f698f45a30966bf20
MurilloTSouza <murillotsouza@gmail.com>
2022-03-10 18:03:49 -0300

    Modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,50 @@
+import React, { useRef } from 'react';
+
+import { CSSTransition } from 'react-transition-group';
+import { IoClose } from 'react-icons/io5';
+
+import './Modal.css';
+
+interface ModalProps {
+    closed?: boolean,
+
+    onClose?: () => void
+}
+
+export const Modal :React.FC<ModalProps> = (props) => {
+
+    const {closed, children} = props;
+
+    const backgroundRef = useRef(null);
+
+    // if not defined just set empty function
+    const onClose = props.onClose
+        ? props.onClose : () => {} ;
+
+    // if click in background div, close Modal
+    const handleBackgroundClick = 
+        (event: React.MouseEvent<HTMLDivElement>) => {
+        if(event.target === backgroundRef.current){
+            onClose();
+        }
+    }
+
+    return (
+        <CSSTransition 
+            in={!closed} 
+            timeout={300} 
+            className="Modal"
+            unmountOnExit>
+            <div ref={backgroundRef} onClick={handleBackgroundClick} >
+
+                <div className='content'>
+                    <IoClose onClick={onClose} id='modal-x'/>
+
+                    {children}
+
+                </div>
+
+            </div>
+        </CSSTransition>
+    )
+}
