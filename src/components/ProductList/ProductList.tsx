1d580540de9a44dcb48a0f92680d5db5b4297969
MurilloTSouza <murillotsouza@gmail.com>
2022-04-01 02:22:59 -0300

    + ProductList

---
+++ src/components/ProductList/ProductList.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Product, ProductCard } from '../ProductCard/ProductCard';
+
+import './ProductList.css';
+
+interface ProductListProps {
+    products: Product[]
+}
+
+export const ProductList :React.FC<ProductListProps> = (props) => {
+
+    const {products} = props;
+
+    var list = products.map( p => 
+        <ProductCard product={p} key={p.id}/>
+    );
+
+    return (
+        <div className='ProductList'>
+            {list}
+        </div>
+    )
+}
